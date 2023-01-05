83d6a90c277a892afc9153fe149aba8da5e44d78
m4dmood <r.maddaloni494@gmail.com>
2023-01-05 18:20:23 +0100

    sqlite implementation and contacts table creation

--- fastifyserver.mjs
+++ fastifyserver.mjs
@@ -1,5 +1,6 @@
 import Fastify from "fastify";
 import { AddressBook } from "./entities/address-book.mjs";
+import * as fastifySqlite from "fastify-sqlite";
 
 /**
  * @param {number} port 
@@ -9,9 +10,26 @@ export function createFastifyServer(port, addressBook) {
 
     const fast = Fastify({ logger: true });
 
+    const db = new fastifySqlite.sqlite3.Database('database.db');
+
+    db.run("CREATE TABLE IF NOT EXISTS contact_cards(id INTEGER PRIMARY KEY AUTOINCREMENT, surname TEXT, name TEXT, telephone TEXT, email TEXT)");
+
     fast.get('/', function (request, response) {
         response.send({ hello: 'world' })
     })
+
+    fast.get('/contacts', function (request, response) {
+        db.all('SELECT * FROM contact_cards', (err, rows) => {
+            response.send(rows);
+        });
+    });
+
+    fast.post('/contacts/new', function (request, response) {
+        db.run('INSERT INTO contact_cards (surname, name, telephone, email) VALUES ($1, $2, $3, $4)',
+        [request.body.surname, request.body.name, request.body.telephone, request.body.email], (err) => {
+            response.send(err);
+        });
+    });
 
     fast.get('/surname/:srn', function(request, response) {
         const requestedSurname = request.params['srn'];
